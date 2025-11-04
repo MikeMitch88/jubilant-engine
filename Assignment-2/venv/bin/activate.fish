885cef50b76a9585e9e6103a61890d6509840fb8
Randa_Mike <michaelranda95@gmail.com>
2025-11-05 00:29:53 +0300

    updates

--- Assignment-2/venv/bin/activate.fish
+++ Assignment-2/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mike_mitch/Downloads/files (2)/venv"
+set -gx VIRTUAL_ENV "/home/mike_mitch/code/jubilant-engine/Assignment-2/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
